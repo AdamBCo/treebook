eeb979e7723297fc00d8f378907ba5b5f035aca4
Adam Cooper <acooper@rallydev.com>
2013-07-05 16:34:45 -0600

    Added users and user friendships with assocatied tests

---
+++ test/unit/user_friendship_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UserFriendshipTest < ActiveSupport::TestCase
+	should belong_to(:user)
+	should belong_to(:friend)
+
+	test "that creating a friendship works without raising an exception" do
+		assert_nothing_raised do
+		UserFriendship.create user: users(:adam), friend: users(:nigel)
+	end
+ end
+
+ 	test "that creating a friendship based on user id and friend id works" do
+ 		UserFriendship.create user_id: users(:adam).id, friend_id: users(:nigel).id
+ 		assert users(:adam).friends.include?(users(:nigel))
+ 	end
+end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	should have_many(:user_friendships)
+	should have_many(:friends)
+
+
 	test "a user should enter a first name" do
 		user = User.new
 		assert !user.save
@@ -45,9 +49,17 @@ class UserTest < ActiveSupport::TestCase
 		assert user.valid?
 	end
 
+	test "that no error is raised when trying to access a friend list" do
+		assert_nothing_raised do
+			users(:adam).friends
+		end
+	end
 
-
-
+	test "that creating friendships on a user works" do
+		users(:adam).friends << users(:nigel)
+		users(:adam).friends.reload
+		assert users(:adam).friends.include?(users(:nigel))
+	end
 end
 
 
